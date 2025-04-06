72694922e2f874805cdd5287509762cfa88a61df
Ladislav Bartoš <ladmeb@gmail.com>
2025-04-06 17:32:08 +0200

    Added clustering leaflet classification into python API

--- pygorder/src/leaflets.rs
+++ pygorder/src/leaflets.rs
@@ -35,12 +35,13 @@ impl<'source> FromPyObject<'source> for LeafletClassification {
             GlobalClassification,
             LocalClassification,
             IndividualClassification,
+            ClusteringClassification,
             ManualClassification,
             NdxClassification
         );
 
         Err(ConfigError::new_err(
-            "expected an instance of GlobalClassification, LocalClassification, IndividualClassification, ManualClassification, or NdxClassification",
+            "expected an instance of GlobalClassification, LocalClassification, IndividualClassification, ClusteringClassification, ManualClassification, or NdxClassification",
         ))
     }
 }
@@ -201,6 +202,31 @@ impl IndividualClassification {
             )?,
             membrane_normal,
         )?;
+
+        Ok(Self(classification))
+    }
+}
+
+/// Assign lipids into leaflets using spectral clustering.
+///
+/// Reliable even for curved membranes but very slow, especially for large systems.
+///
+/// Attributes
+/// ----------
+/// heads : str
+///     Selection query specifying reference atoms representing lipid headgroups
+///     (typically phosphorus atoms or phosphate beads).
+///     There must be exactly one such atom/bead per lipid molecule.
+#[pyclass]
+#[derive(Clone)]
+pub struct ClusteringClassification(RsLeafletClassification);
+
+#[pymethods]
+impl ClusteringClassification {
+    #[new]
+    #[pyo3(signature = (heads, frequency = None))]
+    pub fn new(heads: &str, frequency: Option<Frequency>) -> PyResult<Self> {
+        let classification = add_freq(RsLeafletClassification::clustering(heads), frequency)?;
 
         Ok(Self(classification))
     }
